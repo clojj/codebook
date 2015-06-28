c46ba6adb70e40a2a8d62b71269fdaa88301f2dc
clojj <jwin1968@gmail.com>
2015-06-28 19:38:03 +0200

    added lunr.js

--- app/app.js
+++ app/app.js
@@ -10,3 +10,24 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+
+
+var idx = lunr(function () {
+    this.field('title', { boost: 10 });
+    this.field('body')
+});
+
+var doc = {
+    "title": "Twelfth-Night",
+    "body": "If music be the food of love, play on: Give me excess of it…",
+    "author": "William Shakespeare",
+    "id": 1
+};
+idx.add(doc);
+idx.add({
+    "title": "b",
+    "body": "love is all around",
+    "author": "Me",
+    "id": 2
+});
